f174eab7b2bddd48937646a1729532d56b523bb8
unknown <stevenchrist123@gmail.com>
2022-09-15 14:39:06 +0700

    tambah semuanya

--- wishlist/views.py
+++ wishlist/views.py
@@ -12,6 +12,10 @@ def show_wishlist(request):
     }
     return render(request, "wishlist.html",context)
 
+def show_json(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
 def show_xml(request):
     data = BarangWishlist.objects.all()
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
@@ -19,4 +23,9 @@ def show_xml(request):
 def show_json_by_id(request,id):
     data = BarangWishlist.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request,id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
 
